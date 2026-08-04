49609563585ebc09a59b8f60d4a58e787d65ea84
MuoiTieeu <jackkhi204@outlook.com>
2026-08-04 22:29:30 +0700

    [UPDATE] implement save audio chunk into lowpoly supermarket world

--- scripts/NPCs/NpcController.gd
+++ scripts/NPCs/NpcController.gd
@@ -2,20 +2,23 @@ extends Node3D
 
 @export var npc_audio_player : AudioStreamPlayer3D
 @export var npc_animation_player : AnimationPlayer
+@export var npc_record_indicator: MeshInstance3D
 @export var question_1_audio : AudioStream
 @export var correct_audio: Array[AudioStream]
 @export var wrong_audio: Array[AudioStream]
-
 
 var current_item_id = ""
 var current_hint_audio = null
 var failed_attempts = 0
 static var activeTeacher = null
+var chunkIndex: int = 0
+var isFinalChunk: bool = false 
 
 func _ready():
 	var gameManager = get_node_or_null("/root/GameManager")
 	if gameManager:
 		gameManager.connect("speech_result", Callable(self, "_on_speech_result"))
+	npc_record_indicator.visible = false
 
 # YOU MUST CONNECT THE CASH REGISTER'S 'item_scanned_for_teaching' SIGNAL TO THIS FUNCTION!
 func _on_item_scanned_for_teaching(item_id: String, hint_audio: AudioStream):
@@ -32,7 +35,11 @@ func ask_question_1():
 		npc_audio_player.stream = question_1_audio
 		npc_audio_player.play()
 		await npc_audio_player.finished
-	
+
+	var micController = get_tree().get_first_node_in_group("MicController")
+	if micController:
+		micController.start_chunk_record()
+		npc_record_indicator.visible = true
 	var gameManager = get_node_or_null("/root/GameManager")
 	if gameManager:
 		gameManager.start_checkout_test(current_item_id)
@@ -43,6 +50,10 @@ func ask_question_2():
 		npc_audio_player.play()
 		await npc_audio_player.finished
 	
+	var micController = get_tree().get_first_node_in_group("MicController")
+	if micController:
+		micController.start_chunk_record()
+		npc_record_indicator.visible = true
 	var gameManager = get_node_or_null("/root/GameManager")
 	if gameManager:
 		gameManager.start_checkout_test(current_item_id)
@@ -51,7 +62,15 @@ func _on_speech_result(is_correct: bool):
 	# If this NPC isn't the one who scanned the item, ignore the signal!
 	if activeTeacher != self:
 		return
-		
+	
+	var micController = get_tree().get_first_node_in_group("MicController")
+	if micController:
+		var savedPath = await micController.stop_chunk_record_and_save()
+		npc_record_indicator.visible = false
+		if savedPath != "":
+			isFinalChunk = is_correct or (failed_attempts>=1)
+			FilesChunksApi.UploadChunkAsync(PlayerData.childId, SessionData.sessionId, chunkIndex, savedPath, isFinalChunk, SessionData.accessToken)
+			chunkIndex += 1
 	if is_correct:
 		print("NPC: Correct! Great job!")
 		if npc_audio_player and correct_audio.size() > 0:
