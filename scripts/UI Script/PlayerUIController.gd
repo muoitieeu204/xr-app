aa73fd15d77ecacdea7dc89f32c2ea9a29df9cb5
MuoiTieeu <jackkhi204@outlook.com>
2026-08-16 23:23:58 +0700

    [UPDATE] fix minor bug

--- scripts/UI Script/PlayerUIController.gd
+++ scripts/UI Script/PlayerUIController.gd
@@ -5,6 +5,7 @@ func _ready() -> void:
 	GameManager.score_updated.connect(updated_score)
 	GameManager.time_updated.connect(updated_time)
 	GameManager.item_name_updated.connect(updated_item_name)
+	GameManager.hint_updated.connect(updated_hint)
 
 func updated_score(new_score: int):
 	$MarginContainer/TopHUD/ScoreLabel.text = "Score: " + str(new_score)
@@ -14,3 +15,6 @@ func updated_time(formatted_time: int):
 
 func updated_item_name(new_item_name: String):
 	$MarginContainer/BottomHUD/ItemNameLabel.text = new_item_name
+
+func updated_hint(new_hint: String):
+	$MarginContainer/MiddleHUD/HintLabel.text = new_hint
